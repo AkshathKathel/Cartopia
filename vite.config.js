2d8574f29d0f9bd290e41f4b09e2f3d7bb05fcbd
AkshathKathel <125371442+AkshathKathel@users.noreply.github.com>
2024-11-09 11:55:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Cartopia/',  // Set the base path for GitHub Pages
+  plugins: [react()]
 })
