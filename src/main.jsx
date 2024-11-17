42bea9aadfb26e09bf6df0dd91b46cc48a095960
AkshathKathel <125371442+AkshathKathel@users.noreply.github.com>
2024-11-18 00:04:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import {Home} from './Home.jsx'
 import Booking from './Booking.jsx'
 import Confirmation from './Confirmation.jsx'
 import Clock from './Clock'
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const routes = createBrowserRouter([
   {
@@ -33,7 +32,6 @@ const routes = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router = {routes}/>
-    <SpeedInsights />
     {/* <Clock/> */}
   </React.StrictMode>,
 )
